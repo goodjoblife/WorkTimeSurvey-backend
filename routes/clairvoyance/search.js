bd692f80613d45a15b14e35baabf409222f2f920
Wei-Ming <barry800414@gmail.com>
2016-08-22 14:02:22 +0800

    skeleton of API

--- routes/clairvoyance/search.js
+++ routes/clairvoyance/search.js
@@ -8,7 +8,27 @@ router.get('/by-job', function(req, res, next) {
 });
 
 router.get('/by-company', function(req, res, next) {
-    next();
+    winston.info("/clairvoyance/search/by-company", {company: req.query.company, ip: req.ip, ips: req.ips});
+
+    const company = req.query.company;
+    const collection = req.db.collection('workings');
+
+    if (! company || company === '') {
+        next(new HttpError("company is required", 422));
+        return;
+    }
+    collection.find(
+    	{
+    		$or: [
+            	{'company.name': new RegExp(lodash.escapeRegExp(company.toUpperCase()))},
+                {'company.id': company},
+            ]
+    	}
+    ).toArray().then(function(results) {
+        res.send(results);
+    }).catch(function(err) {
+        next(new HttpError("Internal Server Error", 500));
+    });
 });
 
 module.exports = router;
