e4acc58f970dade94631cb34b5c405e628aa9321
wei-ming <barry800414@gmail.com>
2016-08-22 15:04:59 +0800

    search by company (done)

--- routes/clairvoyance/search.js
+++ routes/clairvoyance/search.js
@@ -1,32 +1,90 @@
 const express = require('express');
 const router = express.Router();
 const HttpError = require('../../libs/errors').HttpError;
+const lodash = require('lodash');
 const winston = require('winston');
 
 router.get('/by-job', function(req, res, next) {
     next();
 });
 
+
+/*
+@param keyword string
+@param [page=0] int (begin from 0)
+@return 200 (每頁有 25 筆的資料)
+{
+  "page": ,// 目前的頁碼
+  "total_page": ,// 總共有幾頁
+  "total_count": ,// 全部頁面有多少的 working 符合搜尋
+  "workings": [
+    {
+      job_title: xx,
+      company: xx,
+      created_at: xx,
+      week_work_time: xx,
+    }
+  ] // Working[]
+}
+@return 422 (如果 keyword 沒有，或是 === "")
+{
+  "error": {},
+  "message": "keyword is required"
+}
+@return 500 (如果內部資料庫連線問題)
+{
+  "error": {},
+  "message": "Internal Server Error"
+}
+*/
 router.get('/by-company', function(req, res, next) {
     winston.info("/clairvoyance/search/by-company", {company: req.query.company, ip: req.ip, ips: req.ips});
 
     const company = req.query.company;
+    const page = req.query.page || 0;
+
     const collection = req.db.collection('workings');
 
     if (! company || company === '') {
         next(new HttpError("company is required", 422));
         return;
     }
-    collection.find(
-    	{
-    		$or: [
-            	{'company.name': new RegExp(lodash.escapeRegExp(company.toUpperCase()))},
+
+    //mongodb query
+    var q = {
+            $or: [
+                {'company.name': new RegExp(lodash.escapeRegExp(company.toUpperCase()))},
                 {'company.id': company},
             ]
-    	}
-    ).toArray().then(function(results) {
-        res.send(results);
+    };
+
+    //sort field
+    var s = {
+        created_at: -1
+    };
+
+    //displayed fields
+    var opt = {
+        _id: 0,
+        job_title:1,
+        company: 1, 
+        created_at:1,
+        week_work_time: 1
+    };
+
+    const data = {};
+
+    collection.find(q).count().then(function(count) {
+        data.total_count = count;
+        data.total_page = Math.ceil(count / 25); 
+        return collection.find(q, opt).sort(s).skip(25 * page).limit(25).toArray();
+    }).then(function(workings) {
+        data.page = page;
+        data.workings = workings;
+
+        res.send(data);
     }).catch(function(err) {
+        console.log(err);
         next(new HttpError("Internal Server Error", 500));
     });
 });
