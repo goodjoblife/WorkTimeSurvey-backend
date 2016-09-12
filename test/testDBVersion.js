8a589f4e816231b6c1e0db02560ac51aabbd1ee8
wei-ming <barry800414@gmail.com>
2016-09-12 16:56:03 +0800

    change promise style again

--- test/testDBVersion.js
+++ test/testDBVersion.js
@@ -1,26 +1,14 @@
 var MongoClient = require('mongodb').MongoClient
 const assert = require('chai').assert;
 
-function connectDB(uri){
-	return new Promise(function(resolve, reject){
-		var conn = MongoClient.connect(uri, function(err, db) {
-			resolve(db.admin());
-		})
-	});
-}
-
-function getVersion(adminDb){
-	return new Promise(function(resolve, reject){
-		adminDb.serverStatus(function(err, info) {
-			resolve(info.version);
-		})
-	});
-}
-
 describe('MongoDB version', function() {
 	it('should be at least 3.x.x', function(done) {
-		connectDB(process.env.MONGODB_URI).then(function(adminDb){
-			return getVersion(adminDb);
+		MongoClient.connect(process.env.MONGODB_URI).then(function(_db) {
+			return new Promise(function(resolve, reject){
+				_db.admin().serverStatus(function(err, info) {
+					resolve(info.version);
+				});
+			});
 		}).then(function(version){
 			var v = parseInt(version.split('.')[0]);
 			assert.isAtLeast(v, 3, 'current version is ' + version);
