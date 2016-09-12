61d3477e5cec7e399200b4985b31182af4d4888e
wei-ming <barry800414@gmail.com>
2016-09-12 11:18:44 +0800

    fix name and tabs and indents

--- test/testDBVersion.js
+++ test/testDBVersion.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient
 const assert = require('chai').assert;
 
-describe('Mongo DB version', function() {
-    it('should be at least 3.x.x', function(done) {
-    	var conn = MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-		    var adminDb = db.admin();
-		    adminDb.serverStatus(function(err, info) {
-		        var version = parseInt(info.version.split('.')[0]);
-		        assert.isAtLeast(version, 3, 'current version is ' + info.version);
-		        done();
-		    })
+describe('MongoDB version', function() {
+	it('should be at least 3.x.x', function(done) {
+		var conn = MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+			var adminDb = db.admin();
+			adminDb.serverStatus(function(err, info) {
+				var version = parseInt(info.version.split('.')[0]);
+				assert.isAtLeast(version, 3, 'current version is ' + info.version);
+				done();
+			})
 		})
-    })
+	})
 })
