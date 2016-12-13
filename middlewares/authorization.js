f247ed70788f990def5f49526c0d906167a16202
peteranny <peter_anny@yahoo.com.tw>
2016-12-13 23:26:15 +0800

    smoothe promise chain

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -62,41 +62,35 @@ function resolveSearchPermission(user_id, db) {
     ])
     // determine authorization
     .then(values => {
-        let sum = values.reduce((a, b) => a+b);
+        const sum = values.reduce((a, b) => a+b);
         if (sum > 0) {
-            return Promise.resolve(true);
+            return true;
         } else {
-            return Promise.resolve(false);
+            return false;
         }
-    }, err => {
-        return Promise.reject(err);
     });
 }
 
 module.exports = (request, response, next) => {
     // redis look up
-    redisLookUp(request.user_id, request.redis_client).
+    redisLookUp(request.user_id, request.redis_client)
     // proceed if user found in cache
-    then(() => {
-        return Promise.resolve();
-    }, err => {
+    .catch(err => {
         // validate user if user not found in cache
         return resolveSearchPermission(request.user_id, request.db)
         // write authorized user into cache for later access
         .then(hasSearchPermission => {
             if (hasSearchPermission) {
-                return redisInsert(request.user_id, request.redis_client).finally(Promise.resolve);
+                return redisInsert(request.user_id, request.redis_client).catch(err => {});
             } else {
                 return Promise.reject('User does not meet authorization level');
             }
-        }, err => {
-            return Promise.reject(err);
         });
     })
     // proceed or throw error
-    .then(hasSearchPermission => {
-        next(hasSearchPermission);
+    .then(() => {
+        next();
     }, err => {
-        throw new HttpError(403, err);
+        throw new HttpError('Forbidden', 403);
     });
 };
