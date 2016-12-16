23fddbbcb57256878c6a0f985d3aa60f16cf183c
Yen-chi Chen <mark86092@gmail.com>
2016-12-17 00:33:15 +0800

    Endpoint POST /me/recommendations done

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const authentication = require('../middlewares/authentication');
+const recommendation = require('../libs/recommendation');
+
+router.post('/me/recommendations', [
+    authentication.cachedFacebookAuthenticationMiddleware,
+    function(req, res, next) {
+        const user = {
+            id: req.facebook.id,
+            type: 'facebook',
+        };
+        recommendation.getRecommendationString(req.db, user).then(recommendation_string => {
+            res.send({
+                user: user,
+                recommendation_string: recommendation_string,
+            });
+        }).catch(err => {
+            next(new HttpError('Internal Server Error', 500));
+        });
+    },
+]);
+
 module.exports = router;
--- test/testMeRecommendations.js
+++ test/testMeRecommendations.js
@@ -7,7 +7,7 @@ const request = require('supertest');
 
 const app = require('../app');
 const recommendation = require('../libs/recommendation');
-const facebook = require('../libs/facebook');
+const authentication = require('../libs/authentication');
 
 describe('POST /me/recommendations 取得使用者推薦字串', function() {
     let sandbox;
@@ -17,19 +17,30 @@ describe('POST /me/recommendations 取得使用者推薦字串', function() {
     });
 
     it('get recommendation string success', function(done) {
-        sandbox.stub(facebook, 'accessTokenAuth').resolves({id: '-1', name: 'mark86092'});
-        sandbox.stub(recommendation, 'getRecommendationString').resolve('00000000ccd8958909a983e9');
+        const cachedFacebookAuthentication = sandbox.stub(authentication, 'cachedFacebookAuthentication')
+            .withArgs(sinon.match.object, 'fakeaccesstoken')
+            .resolves({id: '-1', name: 'mark86092'});
+
+        const getRecommendationString = sandbox.stub(recommendation, 'getRecommendationString')
+            .withArgs(sinon.match.object, {id: '-1', type: 'facebook'})
+            .resolves('00000000ccd8958909a983e9');
 
         request(app).post('/me/recommendations')
+            .send({
+                access_token: 'fakeaccesstoken',
+            })
             .expect(200)
             .expect(function(res) {
+                sinon.assert.calledOnce(cachedFacebookAuthentication);
+                sinon.assert.calledOnce(getRecommendationString);
+
                 assert.propertyVal(res.body, 'recommendation_string', '00000000ccd8958909a983e9');
             })
             .end(done);
     });
 
     it('fail if facebook auth fail', function(done) {
-        sandbox.stub(facebook, 'accessTokenAuth').reject();
+        sandbox.stub(authentication, 'cachedFacebookAuthentication').rejects();
 
         request(app).post('/me/recommendations')
             .expect(401)
