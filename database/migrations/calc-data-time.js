5943476c850bbd3d98b9a2b2c079fa4f9e618205
barry800414 <barry800414@gmail.com>
2017-01-06 14:55:18 +0800

    change to one-by-one executing (not parallel)

--- database/migrations/calc-data-time.js
+++ database/migrations/calc-data-time.js
@@ -1,15 +1,15 @@
 module.exports = (db) => {
     return db.collection('workings').find({}, { created_at: 1 }).toArray()
         .then((data) => {
-            const promiseStack = [];
+            let promiseQueue = Promise.resolve();
             for (let i = 0; i < data.length; i++) {
                 const date = new Date(data[i].created_at);
                 const data_time = {
                     year: date.getFullYear(),
                     month: date.getMonth() + 1,
                 };
-                promiseStack.push(db.collection('workings').update({ _id: data[i]._id }, { $set: { data_time: data_time }}));
+                promiseQueue = promiseQueue.then(db.collection('workings').update({ _id: data[i]._id }, { $set: { data_time: data_time }}));
             }
-            return Promise.all(promiseStack);
+            return promiseQueue;
         });
 };
