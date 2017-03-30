0e092bc7f7b050861b4a0e31340b9a18eca4324b
Wei-Ming <barry800414@gmail.com>
2017-03-30 23:16:15 +0800

    fix testing message and import service of work_experiences.js

--- routes/work_experiences.js
+++ routes/work_experiences.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const ExperienceService = require('../services/experience_service');
 
 router.post('/', function(req, res, next) {
-    //
+    res.send('Yo! you are in POST /work_experiences');
 });
 
 module.exports = router;
