f98aa4468d1c582da671db4561bc60746a3d3d6b
mark <h091237557@gmail.com>
2017-03-31 11:07:38 +0800

    Add: add post replies api and tetst

--- routes/experiences/replies.js
+++ routes/experiences/replies.js
@@ -1,12 +1,34 @@
+const winston = require('winston');
 const express = require('express');
+const HttpError = require('../../libs/errors').HttpError;
 const router = express.Router();
-/* an example to import service
 const ReplyService = require('../../services/reply_service');
-*/
+const authentication = require('../../middlewares/authentication');
 
-router.post('/:id/replies', function(req, res, next) {
-    res.send('Yo! you are in POST /experiences/:id/replies');
-});
+router.post('/:id/replies', [
+    authentication.cachedFacebookAuthenticationMiddleware,
+    function(req, res, next) {
+        const user = {
+            id: req.user.id,
+            type: req.user.type,
+        };
+        const experienceId = req.params.id;
+        const content = req.body.content;
+        const replyService = new ReplyService(req.db);
+        winston.info("/experiences/:id/replies", {
+            id: experienceId,
+            ip: req.ip,
+            ips: req.ips,
+            data: req.body,
+        });
+
+        replyService.addReply(experienceId, user, content).then((result) => {
+            res.send(result);
+        }).catch((err) => {
+            next(new HttpError(err, 500));
+        });
+    },
+]);
 
 router.get('/:id/replies', function(req, res, next) {
     res.send('Yo! you are in GET /experiences/:id/replies');
--- services/reply_service.js
+++ services/reply_service.js
@@ -1,11 +1,64 @@
+const mongo = require('mongodb');
 
 class ReplyService {
 
     constructor(db) {
         this.collection = db.collection('replies');
-        this.experiences_collection = db.collection('experiences');
+        this.experienceCollection = db.collection('experiences');
     }
 
+    /**
+     * 新增留言至工作經驗文章中
+     * @param {string} experienceId - experience's id
+     * @param {object} user - user's object { "id":1111,"type":"facebook" }
+     * @param {string} content - reply content
+     * @returns {Promise}
+     */
+    addReply(experienceId, user, content) {
+        return new Promise((resolve, reject) => {
+            this._checkExperiencedIdExist(experienceId).then((result) => {
+                return this.collection.insertOne({
+                    "experience_id": experienceId,
+                    "user": user,
+                    "created_at": new Date(),
+                    "content": content,
+                    "status": "published",
+                });
+            }).then((result) => {
+                resolve({
+                    "reply": {
+                        "id": result.ops[0]._id.toString(),
+                        "content": content,
+                        "like_count": 0,
+                        "floor": 1,
+                    },
+                });
+            }).catch((err) => {
+                reject({
+                    "msg": "this experienced doesn't exist",
+                });
+            });
+        });
+    }
+
+    /**
+     * 用來驗證要留言的文章是否存在
+     * @return {Promise}
+     */
+    _checkExperiencedIdExist(id) {
+        return new Promise((resolve, reject) => {
+            this.experienceCollection.findOne({
+                "_id": new mongo.ObjectId(id),
+            }, {
+                "_id": 1,
+            }).then((result) => {
+                resolve({
+                    data: result,
+                });
+            });
+        });
+    }
 }
+
 
 module.exports = ReplyService;
