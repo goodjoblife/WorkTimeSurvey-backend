da166bbf94f6ce9ad8adf263a3f74add370e1849
Wei-Ming <barry800414@gmail.com>
2017-03-31 16:40:31 +0800

    add checkIdExist service

--- services/reply_service.js
+++ services/reply_service.js
@@ -1,9 +1,23 @@
+const ObjectId = require('mongodb').ObjectId;
+const ObjectNotExistError = require('../libs/errors').ObjectNotExistError;
 
 class ReplyService {
 
     constructor(db) {
         this.collection = db.collection('replies');
         this.experiences_collection = db.collection('experiences');
+    }
+
+    checkIdExist(id) {
+        return this.collection.findOne({_id: new ObjectId(id)}).then(value => {
+            if(value === null) {
+                throw new ObjectNotExistError("該留言不存在");
+            } else {
+                return value;
+            }
+        }, reason => {
+            throw reason;
+        });
     }
 
 }
