d6d4f0d925f03b435fb4ed59f732c663174f18c1
mark <h091237557@gmail.com>
2017-04-26 21:44:54 +0800

    Add: add 2 test

--- test/api/experiences/testLikes.js
+++ test/api/experiences/testLikes.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert;
+const mongo = new require('mongodb');
 const request = require('supertest');
 const app = require('../../../app');
 const MongoClient = require('mongodb').MongoClient;
@@ -92,16 +93,63 @@ describe('Experience Likes Test', function() {
                     access_token: 'fakeaccesstoken',
                 })
                 .then((res) => {
-                    return request(app)
-                        .get('/experiences/' + experience_id)
-                        .send({
-                            access_token: 'fakeaccesstoken',
-                        })
-                        .expect(200)
-                        .expect((res ) => {
-                            const experience = res.body;
-                            assert.equal(experience.like_count, 1);
+                    return db.collection("experiences")
+                    .find({ _id: new mongo.ObjectId(experience_id)})
+                    .toArray()
+                    .then((result) => {
+                        assert.equal(result[0].like_count, 1);
+                    });
+                });
+        });
+
+        it('Post like 2 times (same user) and get experience , and expected experience like_count will 1 ', function() {
+            const uri = '/experiences/' + experience_id + '/likes';
+            return request(app).post(uri)
+                .send({
+                    access_token: 'fakeaccesstoken',
+                })
+                .then((res) => {
+                    return request(app).post(uri)
+                    .send({
+                        access_token: 'fakeaccesstoken',
+                    });
+                })
+                .then((res) => {
+                    return db.collection("experiences")
+                        .find({ _id: new mongo.ObjectId(experience_id)})
+                        .toArray();
+                })
+                .then((result) => {
+                    assert.equal(result[0].like_count, 1);
+                });
+        });
+
+        it('Post like 2 times(different user) and get experience , and expected experience like_count will 2 ', function() {
+            const uri = '/experiences/' + experience_id + '/likes';
+            return request(app).post(uri)
+                .send({
+                    access_token: 'fakeaccesstoken',
+                })
+                .then((res) => {
+                    sandbox.restore();
+                    sandbox.stub(authentication, 'cachedFacebookAuthentication')
+                        .withArgs(sinon.match.object, 'fakeaccesstoken')
+                        .resolves({
+                            id: '-2',
+                            name: 'markLin002',
                         });
+                    return request(app).post(uri)
+                    .send({
+                        access_token: 'fakeaccesstoken',
+                    });
+                })
+                .then((res) => {
+                    return db.collection("experiences")
+                        .find({ _id: new mongo.ObjectId(experience_id)})
+                        .toArray();
+                })
+                .then((result) => {
+                    assert.equal(result[0].like_count, 2);
                 });
         });
 
