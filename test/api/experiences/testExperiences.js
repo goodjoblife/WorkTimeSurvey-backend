bc58c973fbd776e23b7b38f2c6986ce46b78f522
Yen-chi Chen <mark86092@gmail.com>
2017-05-12 14:39:26 +0800

    Revert

--- test/api/experiences/testExperiences.js
+++ test/api/experiences/testExperiences.js
@@ -168,126 +168,139 @@ describe('Experiences 面試和工作經驗資訊', function() {
             sandbox.restore();
         });
     });
+
     describe('GET /experiences', function() {
 
         before('Seeding some experiences', function() {
-            return db.collection('experiences').insertMany([{
-                type: "interview",
-                created_at: new Date("2017-03-20T10:00:00.929Z"),
-                company: {
-                    name: "GOODJOB1",
-                    id: "123",
+            return db.collection('experiences').insertMany([
+                {
+                    type: "interview",
+                    created_at: new Date("2017-03-20T10:00:00.929Z"),
+                    company: {
+                        name: "GOODJOB1",
+                        id: "123",
+                    },
+                    area: "台北",
+                    title: "Good面試",
+                    job_title: "SW ENGINEER",
+                    overall_rating: "5",
+                    sections: [
+                        {
+                            subtitle: "面試過程",
+                            content: "很開心",
+                        },
+                    ],
+                    interview_time: {
+                        year: 10,
+                        month: 1,
+                    },
+                    interview_qas: [
+                        { question: "What your name?", answer: "I'm Mark" },
+                    ],
+                    interview_result: "Sorry ~ ",
+                    interview_sensitive_questions: [
+                        "Are you a yacht boy ?",
+                    ],
+                    salary: {
+                        type: "year",
+                        amount: 1000000,
+                    },
+                    experience_in_year: "1",
+                    education: "bachelor",
+                    salary_type: "month",
+                    salary_amount: "66666",
+                    like_count: 1,
+                    reply_count: 1,
                 },
-                area: "台北",
-                title: "Good面試",
-                job_title: "SW ENGINEER",
-                overall_rating: "5",
-                sections: [{
-                    subtitle: "面試過程",
-                    content: "很開心",
-                }],
-                interview_time: {
-                    year: 10,
-                    month: 1,
+                {
+                    type: "work",
+                    author: {
+                        type: "facebook",
+                        _id: "abcde",
+                    },
+                    created_at: new Date("2017-03-21T10:00:00.929Z"),
+                    company: {
+                        name: "GOODJOB2",
+                        id: "123",
+                    },
+                    region: "台北",
+                    job_title: "ENGINEER",
+                    title: "Facebook Work",
+                    sections: [
+                        {
+                            subtitle: "面試過程",
+                            content: "很開心",
+                        },
+                    ],
+                    experience_in_year: "1",
+                    education: "bachelor",
+                    is_currently_employed: "yes",
+                    job_ending_time: {
+                        year: 2017,
+                        month: 1,
+                    },
+                    salary: {
+                        type: "month",
+                        amount: 100000,
+                    },
+                    week_work_time: 40,
+                    recommend_to_others: "yes",
+                    data_time: {
+                        year: 2017,
+                        month: 1,
+                    },
+                    like_count: 1,
+                    reply_count: 1,
                 },
-                interview_qas: [{
-                    question: "What your name?",
-                    answer: "I'm Mark",
-                }],
-                interview_result: "Sorry ~ ",
-                interview_sensitive_questions: [
-                    "Are you a yacht boy ?",
-                ],
-                salary: {
-                    type: "year",
-                    amount: 1000000,
+                {
+                    type: "interview",
+                    created_at: new Date("2017-03-22T10:00:00.929Z"),
+                    company: {
+                        name: "BADJOB",
+                        id: "321",
+                    },
+                    area: "台北",
+                    job_title: "HW ENGINEER",
+                    interview_time_year: "2017",
+                    interview_time_month: "3",
+                    // interview_result: ???,
+                    overall_rating: "5",
+                    sections: [
+                        {
+                            subtitle: "面試過程",
+                            content: "很開心",
+                        },
+                    ],
+                    experience_in_year: "1",
+                    education: "bachelor",
+                    salary_type: "month",
+                    salary_amount: "77777",
                 },
-                experience_in_year: "1",
-                education: "bachelor",
-                salary_type: "month",
-                salary_amount: "66666",
-                like_count: 1,
-                reply_count: 1,
-            }, {
-                type: "work",
-                author: {
-                    type: "facebook",
-                    _id: "abcde",
+                {
+                    type: "work",
+                    created_at: new Date("2017-03-25T10:00:00.929Z"),
+                    company: {
+                        name: "GOODJOB1",
+                        id: "321",
+                    },
+                    area: "台北",
+                    job_title: "F2E",
+                    interview_time_year: "2017",
+                    interview_time_month: "3",
+                    // interview_result: ???,
+                    overall_rating: "5",
+                    sections: [
+                        {
+                            subtitle: "面試過程",
+                            content: "很開心",
+                        },
+                    ],
+                    experience_in_year: "1",
+                    education: "bachelor",
+                    salary_type: "month",
+                    salary_amount: "77777",
                 },
-                created_at: new Date("2017-03-21T10:00:00.929Z"),
-                company: {
-                    name: "GOODJOB2",
-                    id: "123",
-                },
-                region: "台北",
-                job_title: "ENGINEER",
-                title: "Facebook Work",
-                sections: [{
-                    subtitle: "面試過程",
-                    content: "很開心",
-                }],
-                experience_in_year: "1",
-                education: "bachelor",
-                is_currently_employed: "yes",
-                job_ending_time: {
-                    year: 2017,
-                    month: 1,
-                },
-                salary: {
-                    type: "month",
-                    amount: 100000,
-                },
-                week_work_time: 40,
-                recommend_to_others: "yes",
-                data_time: {
-                    year: 2017,
-                    month: 1,
-                },
-                like_count: 1,
-                reply_count: 1,
-            }, {
-                type: "interview",
-                created_at: new Date("2017-03-22T10:00:00.929Z"),
-                company: {
-                    name: "BADJOB",
-                    id: "321",
-                },
-                area: "台北",
-                job_title: "HW ENGINEER",
-                interview_time_year: "2017",
-                interview_time_month: "3",
-                // interview_result: ???,
-                overall_rating: "5",
-                sections: [{
-                    subtitle: "面試過程",
-                    content: "很開心",
-                }],
-                experience_in_year: "1",
-                education: "bachelor",
-                salary_type: "month",
-                salary_amount: "77777",
-            }, {
-                type: "work",
-                created_at: new Date("2017-03-25T10:00:00.929Z"),
-                company: {
-                    name: "GOODJOB1",
-                    id: "321",
-                },
-                area: "台北",
-                job_title: "F2E",
-                interview_time_year: "2017",
-                interview_time_month: "3",
-                // interview_result: ???,
-                overall_rating: "5",
-                sections: [{
-                    subtitle: "面試過程",
-                    content: "很開心",
-                }],
-                experience_in_year: "1",
-                education: "bachelor",
-                salary_type: "month",
-                salary_amount: "77777",
-            }]);
+            ]);
         });
 
         it(`check API return correct data without query`, function() {
