9dfb063c5d912ac79186462bdcf8dcf2d1f530b2
mark <h091237557@gmail.com>
2017-05-16 23:01:18 +0800

    fix : fix circle ci  error

--- test/api/experiences/testLikes.js
+++ test/api/experiences/testLikes.js
@@ -2,10 +2,7 @@ const assert = require('chai').assert;
 const mongo = new require('mongodb');
 const request = require('supertest');
 const app = require('../../../app');
-const {
-    MongoClient,
-    ObjectId,
-} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const sinon = require('sinon');
 require('sinon-as-promised');
 const config = require('config');
