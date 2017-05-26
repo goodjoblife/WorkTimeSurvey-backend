c8919c593420871e81ec602dff7236a34b38ae89
Wei-Ming <barry800414@gmail.com>
2017-05-26 13:27:13 +0800

    fix variable name

--- routes/workings_post.js
+++ routes/workings_post.js
@@ -360,9 +360,9 @@ function main(req, res, next) {
             amount: req.custom.salary_amount,
         };
 
-        const wage = helper.calculateEstimatedHourlyWage(working);
-        if (typeof wage !== 'undefined') {
-            working.estimated_hourly_wage = wage;
+        const estimated_hourly_wage = helper.calculateEstimatedHourlyWage(working);
+        if (typeof estimated_hourly_wage !== 'undefined') {
+            working.estimated_hourly_wage = estimated_hourly_wage;
         }
     }
     if (working.is_currently_employed === 'no') {
