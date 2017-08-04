e0741c80b72e325d9b1be8f3e4a29985d201fdb5
Yen-chi Chen <mark86092@gmail.com>
2017-08-05 00:26:50 +0800

    this test may fail / success due to async, need rewrite

--- test/api/job_title_keywords/testJobTitleKeywords.js
+++ test/api/job_title_keywords/testJobTitleKeywords.js
@@ -27,25 +27,6 @@ describe('Job title Keywords Test', function () {
         });
     });
 
-    describe('Get : /experiences (key word check)', function () {
-        it('should return 200', function () {
-            const query = (new ObjectId()).toString();
-            return request(app).get('/experiences')
-                .query({
-                    search_query: query,
-                    search_by: 'job_title',
-                })
-                .expect(200)
-                .then(() => db.collection('job_title_keywords')
-                        .findOne({
-                            word: query.toString(),
-                        }))
-                .then((result) => {
-                    assert.equal(result.word, query.toString());
-                });
-        });
-    });
-
     after(function () {
         return db.collection('job_title_keywords').drop()
             .then(() => create_capped_collection(db));
