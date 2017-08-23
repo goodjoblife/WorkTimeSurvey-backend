66f4145e4a9f14c32934085997616ed917cdbafe
Yen-chi Chen <mark86092@gmail.com>
2017-08-23 13:26:51 +0800

    移動 /workings API 相關至資料夾

--- routes/workings/index.js
+++ routes/workings/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const HttpError = require('../libs/errors').HttpError;
+const HttpError = require('../../libs/errors').HttpError;
 const escapeRegExp = require('lodash/escapeRegExp');
 const post_helper = require('./workings_post');
-const middleware = require('./middleware');
+const middleware = require('../middleware');
 const passport = require('passport');
 
 router.get('/', middleware.sort_by);
