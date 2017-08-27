e7d2f3d8da2f594ca2b431f32c9f702a7b47e233
mark <h091237557@gmail.com>
2017-08-27 15:02:37 +0800

    Add the get workings view model

---
+++ view_models/get_workings.js
@@ -0,0 +1,22 @@
+
+function generateGetWorkingsViewModel(workings, total) {
+    const view_workings = workings.map(working => ({
+        company: workings.company,
+        sector: workings.sector,
+        created_at: working.created_at,
+        data_time: working.data_time,
+        estimated_hourly_wage: working.estimated_hourly_wage,
+        job_title: working.job_title,
+        overtime_frequency: working.overtime_frequency,
+        salary: working.salary,
+        week_work_time: working.week_work_time,
+    }));
+
+    const result = {
+        total,
+        time_and_salary: view_workings,
+    };
+
+    return result;
+}
+module.exports = generateGetWorkingsViewModel;
