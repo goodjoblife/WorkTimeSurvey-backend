e02da3eed38e886e4dcb36b3a44dcea25eaad5ff
mark <h091237557@gmail.com>
2017-08-27 15:10:34 +0800

    Add the get /me/workings route

---
+++ routes/me/workings/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+const wrap = require('../../../libs/wrap');
+const generateGetWorkingsViewModel = require('../../../view_models/get_workings');
+const passport = require('passport');
+const WorkingModel = require('../../../models/working_model');
+
+
+router.get('/', [
+    passport.authenticate('bearer', { session: false }),
+    wrap(async (req, res) => {
+        const user = req.user;
+        const query = {
+            "author._id": user._id,
+        };
+
+        const working_model = new WorkingModel(req.db);
+        const count = await working_model.getWorkingsCountByQuery(query);
+        const workings = await working_model.getWorkings(query);
+
+        res.send(generateGetWorkingsViewModel(workings, count));
+    })]
+);
+
+module.exports = router;
