9815bcda15789dfaed540577e7ea10dfae5eff25
LittleWhiteYA <f46284628@gmail.com>
2019-02-24 11:50:51 +0800

    rewrite search_companies and schema

--- schema/company.js
+++ schema/company.js
@@ -1,6 +1,7 @@
+const { gql } = require("apollo-server-express");
 const escapeRegExp = require("lodash/escapeRegExp");
 
-const Type = `
+const Type = gql`
     type Company {
         name: String!
 
@@ -10,13 +11,9 @@ const Type = `
     }
 `;
 
-const Query = `
+const Query = gql`
     extend type Query {
-        search_companies(query: String!): SearchCompanyPayload!
-    }
-
-    type SearchCompanyPayload {
-        companies: [Company!]!
+        search_companies(query: String!): [Company!]!
     }
 `;
 
@@ -25,6 +22,7 @@ const resolvers = {
         async search_companies(_, { query: company }, ctx) {
             const collection = ctx.db.collection("workings");
 
+            // FIXME: should query from collection `companies`
             const results = await collection
                 .aggregate([
                     {
@@ -48,6 +46,66 @@ const resolvers = {
                     {
                         $group: {
                             _id: "$company",
+                        },
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            id: "$_id.id",
+                            name: "$_id.name",
+                        },
+                    },
+                ])
+                .toArray();
+
+            return results;
+        },
+    },
+    Company: {
+        salary_work_times: async (company, _, ctx) => {
+            const collection = ctx.db.collection("workings");
+
+            const orExpr = [{ "company.name": company.name }];
+
+            if (company.id) {
+                orExpr.push({
+                    "company.id": company.id,
+                });
+            }
+
+            const workTimes = await collection
+                .find({
+                    status: "published",
+                    "archive.is_archived": false,
+                    $or: orExpr,
+                })
+                .toArray();
+
+            return workTimes;
+        },
+        salary_work_time_statistics: async (company, _, ctx) => {
+            const collection = ctx.db.collection("workings");
+
+            const orExpr = [{ "company.name": company.name }];
+
+            if (company.id) {
+                orExpr.push({
+                    "company.id": company.id,
+                });
+            }
+
+            const statistics = await collection
+                .aggregate([
+                    {
+                        $match: {
+                            status: "published",
+                            "archive.is_archived": false,
+                            $or: orExpr,
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: null,
                             has_overtime_salary_yes: {
                                 $sum: {
                                     $cond: [
@@ -180,80 +238,51 @@ const resolvers = {
                     },
                     {
                         $project: {
-                            _id: 0,
-                            id: "$_id.id",
-                            name: "$_id.name",
-                            salary_work_time_statistics: {
-                                count: "$count",
-                                average_week_work_time: "$avg_week_work_time",
-                                average_estimated_hourly_wage:
-                                    "$avg_estimated_hourly_wage",
-                                has_overtime_salary_count: {
-                                    $cond: [
-                                        { $gte: ["$count", 5] },
-                                        {
-                                            yes: "$has_overtime_salary_yes",
-                                            no: "$has_overtime_salary_no",
-                                            unknown:
-                                                "$has_overtime_salary_dont",
-                                        },
-                                        "$skip",
-                                    ],
-                                },
-                                is_overtime_salary_legal_count: {
-                                    $cond: [
-                                        { $gte: ["$count", 5] },
-                                        {
-                                            yes:
-                                                "$is_overtime_salary_legal_yes",
-                                            no: "$is_overtime_salary_legal_no",
-                                            unknown:
-                                                "$is_overtime_salary_legal_dont",
-                                        },
-                                        "$skip",
-                                    ],
-                                },
-                                has_compensatory_dayoff_count: {
-                                    $cond: [
-                                        { $gte: ["$count", 5] },
-                                        {
-                                            yes: "$has_compensatory_dayoff_yes",
-                                            no: "$has_compensatory_dayoff_no",
-                                            unknown:
-                                                "$has_compensatory_dayoff_dont",
-                                        },
-                                        "$skip",
-                                    ],
-                                },
+                            count: 1,
+                            average_week_work_time: "$avg_week_work_time",
+                            average_estimated_hourly_wage:
+                                "$avg_estimated_hourly_wage",
+                            has_overtime_salary_count: {
+                                $cond: [
+                                    { $gte: ["$count", 5] },
+                                    {
+                                        yes: "$has_overtime_salary_yes",
+                                        no: "$has_overtime_salary_no",
+                                        unknown: "$has_overtime_salary_dont",
+                                    },
+                                    "$skip",
+                                ],
+                            },
+                            is_overtime_salary_legal_count: {
+                                $cond: [
+                                    { $gte: ["$count", 5] },
+                                    {
+                                        yes: "$is_overtime_salary_legal_yes",
+                                        no: "$is_overtime_salary_legal_no",
+                                        unknown:
+                                            "$is_overtime_salary_legal_dont",
+                                    },
+                                    "$skip",
+                                ],
+                            },
+                            has_compensatory_dayoff_count: {
+                                $cond: [
+                                    { $gte: ["$count", 5] },
+                                    {
+                                        yes: "$has_compensatory_dayoff_yes",
+                                        no: "$has_compensatory_dayoff_no",
+                                        unknown:
+                                            "$has_compensatory_dayoff_dont",
+                                    },
+                                    "$skip",
+                                ],
                             },
                         },
                     },
                 ])
                 .toArray();
 
-            return {
-                companies: results,
-            };
-        },
-    },
-    Company: {
-        salary_work_times: async (company, _, ctx) => {
-            const collection = ctx.db.collection("workings");
-
-            const workTimes = await collection
-                .find({
-                    $or: [
-                        {
-                            "company.name": company.name,
-                        },
-                        {
-                            "company.id": company.id,
-                        },
-                    ],
-                })
-                .toArray();
-
-            return workTimes;
+            return statistics[0];
         },
     },
 };
