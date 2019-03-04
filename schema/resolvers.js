a25f4693ba19122f4a5c2dc491b2a42a953c376c
Yen-chi Chen <mark86092@gmail.com>
2019-03-04 13:45:14 +0800

    Schema draft

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -2,8 +2,13 @@ const R = require("ramda");
 const merge = R.reduce(R.mergeDeepLeft, {});
 
 module.exports = merge([
+    require("./company").resolvers,
     require("./company_keyword").resolvers,
+    require("./experience").resolvers,
+    require("./job_title").resolvers,
     require("./job_title_keyword").resolvers,
     require("./me").resolvers,
+    require("./reply").resolvers,
+    require("./salary_work_time").resolvers,
     require("./user").resolvers,
 ]);
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -15,8 +15,13 @@ const Mutation = gql`
 module.exports = [
     Query,
     Mutation,
+    ...require("./company").types,
     ...require("./company_keyword").types,
+    ...require("./experience").types,
+    ...require("./job_title").types,
     ...require("./job_title_keyword").types,
     ...require("./me").types,
+    ...require("./reply").types,
+    ...require("./salary_work_time").types,
     ...require("./user").types,
 ];
