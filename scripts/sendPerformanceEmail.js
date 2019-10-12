99143b31353d6f2cf8a2a3f6794982e427027710
LittleWhiteYA <f46284628@gmail.com>
2019-10-12 15:54:51 +0800

    rename workings -> experiences and remove unexisted column

--- scripts/sendPerformanceEmail.js
+++ scripts/sendPerformanceEmail.js
@@ -5,7 +5,6 @@ const { ObjectId } = require("mongodb");
 
 const { connectMongo } = require("../src/models/connect");
 const ExperienceModel = require("../src/models/experience_model");
-const ModelManager = require("../src/models/manager");
 
 const {
     ExperienceViewLogNotificationTemplate,
@@ -17,8 +16,6 @@ const GOODJOB_DOMAIN = "https://www.goodjob.life";
 (async () => {
     const { db, client } = await connectMongo();
     const experienceModel = new ExperienceModel(db);
-    const manager = new ModelManager(db);
-    const { UserModel } = manager;
 
     // order is important
     const viewCountThreshold = [1000, 500, 100];
@@ -35,104 +32,161 @@ const GOODJOB_DOMAIN = "https://www.goodjob.life";
                         },
                     },
                     {
-                        $group: {
-                            _id: "$author_id",
-                            workings: {
-                                $push: {
-                                    _id: "$_id",
-                                    title: "$title",
-                                    content: "$content",
-                                    url: {
-                                        $concat: [
-                                            `${GOODJOB_DOMAIN}/experiences/`,
-                                            { $toString: "$_id" },
-                                        ],
-                                    },
-                                    viewCount: "$view_count",
-                                    type: "$type",
-                                    sections: "$sections",
-                                },
-                            },
+                        $lookup: {
+                            from: "users",
+                            localField: "author_id",
+                            foreignField: "_id",
+                            as: "user",
                         },
                     },
                     {
-                        $lookup: {
-                            from: "users",
-                            localField: "_id",
-                            foreignField: "_id",
-                            as: "user",
+                        $match: {
+                            "user.subscribeEmail": true,
+                            "user.email": { $exists: true },
                         },
                     },
                     {
                         $unwind: "$user",
                     },
                     {
-                        $match: {
-                            "user.subscribeEmail": true,
-                            "user.email": { $exists: true },
-                            $or: [
-                                {
-                                    // before 14 days
-                                    "user.last_email_time": {
-                                        $lt: new Date(
-                                            new Date() -
-                                                1000 * 60 * 60 * 24 * 14
-                                        ),
-                                    },
+                        $lookup: {
+                            from: "email_logs",
+                            localField: "user._id",
+                            foreignField: "user_id",
+                            as: "user.email_logs",
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: "$author_id",
+                            experiences: {
+                                $push: {
+                                    _id: "$_id",
+                                    title: "$title",
+                                    viewCount: "$view_count",
+                                    type: "$type",
+                                    sections: "$sections",
                                 },
-                                { "user.last_email_time": { $exists: false } },
-                            ],
+                            },
+                            user: {
+                                $first: "$user",
+                            },
                         },
                     },
                 ])
                 .toArray();
 
-            const template = new ExperienceViewLogNotificationTemplate();
+            const userWithNewExperiences = userWithExperiences
+                .filter(userWithExperience => {
+                    const {
+                        user: { email_logs: emailLogs },
+                    } = userWithExperience;
 
-            const emailInfos = userWithExperiences.map(userWithExperience => {
-                // TODO: maybe send multiple experiences in one email
-                const experience = userWithExperience.workings[0];
-                const content = experience.sections
-                    .map(section => section.content)
-                    .join("\n");
+                    return (
+                        emailLogs.length === 0 ||
+                        // 全部的 email logs 都是兩週前的
+                        emailLogs.every(
+                            log =>
+                                new Date(log.created_at) <
+                                new Date(new Date() - 1000 * 60 * 60 * 24 * 14)
+                        )
+                    );
+                })
+                .map(userWithExperience => {
+                    const {
+                        experiences,
+                        user: { email_logs: emailLogs },
+                    } = userWithExperience;
 
-                let typeName;
-                if (experience.type === "intern") {
-                    typeName = "實習心得";
-                } else if (experience.type === "interview") {
-                    typeName = "面試經驗";
-                } else {
-                    typeName = "工作心得";
-                }
+                    // all experiences sent before
+                    const oldExperiences = emailLogs.map(
+                        emailLog => emailLog.reason
+                    );
 
-                return {
-                    email: userWithExperience.user.email,
-                    userId: userWithExperience.user._id,
-                    subject: {
-                        username: userWithExperience.user.name,
-                        experience: {
-                            title: experience.title,
-                            viewCount: experience.viewCount,
-                            url: experience.url,
-                            typeName,
-                            content,
+                    const newExperiences = experiences
+                        .filter(experience =>
+                            oldExperiences.every(
+                                old =>
+                                    // experience 以前從沒寄過信
+                                    old.experience_id.toString() !==
+                                        experience._id.toString() ||
+                                    // experience 有被寄過信，但是之前的 threshold 都比較小
+                                    (old.experience_id.toString() ===
+                                        experience._id.toString() &&
+                                        old.threshold < experience.viewCount &&
+                                        old.threshold !== threshold)
+                            )
+                        )
+                        .map(experience => ({
+                            ...experience,
+                            url: `${GOODJOB_DOMAIN}/experiences/${experience._id.toString()}`,
+                        }));
+
+                    return {
+                        ...userWithExperience,
+                        experiences: newExperiences,
+                    };
+                })
+                .filter(
+                    userWithExperience =>
+                        userWithExperience.experiences.length > 0
+                );
+
+            const emailInfos = userWithNewExperiences.map(
+                userWithExperience => {
+                    // TODO: maybe send multiple experiences in one email
+                    const experience = userWithExperience.experiences[0];
+                    const content = experience.sections
+                        .map(section => section.content)
+                        .join("\n");
+
+                    let typeName;
+                    if (experience.type === "intern") {
+                        typeName = "實習心得";
+                    } else if (experience.type === "interview") {
+                        typeName = "面試經驗";
+                    } else {
+                        typeName = "工作心得";
+                    }
+
+                    return {
+                        email: userWithExperience.user.email,
+                        emailLogs: {
+                            userId: userWithExperience.user._id,
+                            experienceId: experience._id,
                         },
-                    },
-                };
-            });
+                        subject: {
+                            username: userWithExperience.user.name,
+                            experience: {
+                                title: experience.title,
+                                viewCount: experience.viewCount,
+                                url: experience.url,
+                                typeName,
+                                content,
+                            },
+                        },
+                    };
+                }
+            );
+
+            const template = new ExperienceViewLogNotificationTemplate();
 
             await pMap(
                 emailInfos,
                 async emailInfo => {
-                    const { userId, email, subject } = emailInfo;
+                    const { emailLogs, email, subject } = emailInfo;
                     const current = new Date();
 
                     await sendEmailsFromTemplate([email], template, subject);
 
-                    await UserModel.collection.updateOne(
-                        { _id: ObjectId(userId) },
-                        { $set: { last_email_time: current } }
-                    );
+                    await db.collection("email_logs").insertOne({
+                        user_id: ObjectId(emailLogs.userId),
+                        created_at: current,
+                        reason: {
+                            experience_id: ObjectId(emailLogs.experienceId),
+                            threshold,
+                        },
+                    });
                 },
                 { concurrency: 10 }
             );
