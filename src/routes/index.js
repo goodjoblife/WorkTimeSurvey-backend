d3c20ab4905b07550637316f865b2f9a2e22b2df
Wei Ming Chen <barry800414@gmail.com>
2020-02-24 00:25:49 +0800

    remove POST /interview_experiences

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 // please sort in alphabetical order
 router.use("/companies", require("./companies"));
 router.use("/experiences", require("./experiences"));
-router.use("/interview_experiences", require("./interview_experiences"));
 router.use("/jobs", require("./jobs"));
 router.use("/me", require("./me"));
 router.use("/replies", require("./replies"));
