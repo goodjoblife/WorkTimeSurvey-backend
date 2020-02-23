9399dc1a449f15dc13ef34ef4753c2ad6af69038
Wei Ming Chen <barry800414@gmail.com>
2020-02-24 00:28:15 +0800

    remove /jobs/search

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 // please sort in alphabetical order
 router.use("/companies", require("./companies"));
 router.use("/experiences", require("./experiences"));
-router.use("/jobs", require("./jobs"));
 router.use("/me", require("./me"));
 router.use("/replies", require("./replies"));
 router.use("/workings", require("./workings"));
