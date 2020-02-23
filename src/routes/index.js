4461208db1bb9d04deba645fbfc857bf5435b55c
Wei Ming Chen <barry800414@gmail.com>
2020-02-24 00:25:10 +0800

    remove POST /auth/facebook & POST /auth/google

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 // please sort in alphabetical order
-router.use("/auth", require("./auth"));
 router.use("/companies", require("./companies"));
 router.use("/experiences", require("./experiences"));
 router.use("/interview_experiences", require("./interview_experiences"));
