8243f0e0c2b16b07c8044d7dec8995238b23cb24
Wei Ming Chen <barry800414@gmail.com>
2020-07-12 11:42:43 +0800

    feat: me.unlocked_experiences, me.unlocked_salary_work_times, me.points

--- src/schema/me.js
+++ src/schema/me.js
@@ -6,7 +6,6 @@ const {
     UserPointEvent,
     COMPLETED,
 } = require("../models/schemas/userPointEvent");
-const User = require("../models/schemas/userModel");
 const ExperienceModel = require("../models/experience_model");
 const {
     unlockExperience,
@@ -20,14 +19,6 @@ const Type = `
 const Query = gql`
     extend type Query {
         me: User!
-        "取得已經解鎖的職場經驗列表"
-        unlocked_experiences: [Experience!]
-        "取得已經解鎖的薪資工時列表"
-        unlocked_salary_work_times: [SalaryWorkTime!]
-        "目前擁有的積分"
-        points: Int!
-        "取得該使用者的點數事件（完成任務或兌換獎勵）"
-        pointEvents: [UserPointEvent!]
     }
 `;
 
@@ -46,55 +37,6 @@ const resolvers = {
             const user = context.user;
             return user;
         }),
-        unlocked_experiences: combineResolvers(
-            isAuthenticated,
-            async (root, args, context) => {
-                const user = context.user;
-                const records = await UserPointEvent.find({
-                    user_id: user._id,
-                    event_name: unlockExperience,
-                    status: COMPLETED,
-                });
-                if (!records) {
-                    return [];
-                }
-                return (await context.db.collection("experiences").find({
-                    _id: { $in: records.map(el => el.doc_id) },
-                })).toArray();
-            }
-        ),
-        unlocked_salary_work_times: combineResolvers(
-            isAuthenticated,
-            async (root, args, context) => {
-                const user = context.user;
-                const records = await UserPointEvent.find({
-                    user_id: user._id,
-                    event_name: unlockSalaryWorkTime,
-                    status: COMPLETED,
-                });
-                if (!records) {
-                    return [];
-                }
-                return (await context.db.collection("experiences").find({
-                    _id: { $in: records.map(el => el.doc_id) },
-                })).toArray();
-            }
-        ),
-        points: combineResolvers(
-            isAuthenticated,
-            async (root, args, context) => {
-                return (await User.findById(context.user._id)).points;
-            }
-        ),
-        pointEvents: combineResolvers(
-            isAuthenticated,
-            async (root, args, context) => {
-                const user = context.user;
-                return await UserPointEvent.find({
-                    user_id: user._id,
-                });
-            }
-        ),
     },
     Mutation: {
         unlockExperience: combineResolvers(
