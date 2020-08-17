d8d9f2d47231cc2dba5ee939c2581aaea1b98d97
Wei Ming Chen <barry800414@gmail.com>
2020-08-17 20:03:33 +0800

    feat: update points of each event

--- src/libs/events/reward_config.js
+++ src/libs/events/reward_config.js
@@ -4,12 +4,12 @@ const rewardConfigMap = {
     [unlockExperience]: {
         title: "解鎖面試、工作心得",
         description: "每篇 100 積分",
-        points: 1,
+        points: 100,
     },
     [unlockSalaryWorkTime]: {
         title: "解鎖一筆薪水、加班資料",
         description: "每筆 50 積分",
-        points: 1,
+        points: 50,
     },
 };
 
--- src/libs/events/task_config.js
+++ src/libs/events/task_config.js
@@ -9,25 +9,25 @@ const taskConfigMap = {
     [createSalaryWorkTime]: {
         title: "留下一筆薪資工時",
         description: "+ 100 積分",
-        points: 1,
+        points: 100,
         maxRunCount: 0,
     },
     [createInterviewExperience]: {
         title: "寫一篇面試心得",
         description: "至少 + 100 積分",
-        points: 1,
+        points: 100,
         maxRunCount: 0,
     },
     [createWorkExperience]: {
         title: "寫一篇工作心得",
         description: "至少 + 100 積分",
-        points: 1,
+        points: 100,
         maxRunCount: 0,
     },
     [shareWebsite]: {
         title: "分享我們的網站",
         description: "+ 100 積分",
-        points: 1,
+        points: 100,
         maxRunCount: 1,
     },
 };
